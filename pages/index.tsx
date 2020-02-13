3c0bd522df821d1e5fe2d003943f1261856551be
Youhei Nozaki <youhey.hey0505@gmail.com>
2020-02-13 15:15:25 +0900

    hello next.js

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import Link from 'next/link';
+import { NextPage } from 'next';
+
+const IndexPage: NextPage = () => {
+  return (
+    <>
+      <h1>Hello Next.js</h1>
+      <p>
+        <Link href="/about">
+          <a>About</a>
+        </Link>
+      </p>
+    </>
+  );
+};
+
+export default IndexPage;
