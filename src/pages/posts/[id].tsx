18e0e99bbbf22e223739b8a0073e6a85cbe5a407
Youhei Nozaki <youhey.hey0505@gmail.com>
2020-02-21 20:54:14 +0900

    各ページのOGPを設定

--- src/pages/posts/[id].tsx
+++ src/pages/posts/[id].tsx
@@ -7,6 +7,7 @@ import { axiosInstance } from '../../lib/api';
 import { IPost } from '../../interfaces';
 
 import Layout from '../../components/templates/Layout';
+import HeadComponent from '../../components/templates/Head';
 
 type Props = {
   post: IPost;
@@ -14,29 +15,38 @@ type Props = {
 
 const PostContent: NextPage<Props> = ({ post }) => {
   return (
-    <Layout>
-      <Heading fontSize={{ sm: '3xl' }} margin={2} marginLeft={4}>
-        {post.title}
-      </Heading>
-      <div>
-        {post.tags.map(tag => (
-          <React.Fragment key={tag.id}>
-            <Tag marginLeft={6} marginTop={1} size={'sm'}>
-              {tag.name}
-            </Tag>
-          </React.Fragment>
-        ))}
-      </div>
-      <Image
-        src={post.image.url}
-        rounded="lg"
-        margin={4}
-        marginBottom={[10, 20, 20, 20]}
+    <>
+      <HeadComponent
+        title={post.title}
+        description={post.content}
+        keyword={post.title}
+        image={post.image.url}
+        url={`https://ryusou-blog.now.sh/posts/${post.id}`}
       />
-      <Flex margin="6">
-        <div dangerouslySetInnerHTML={{ __html: `${post.content}` }}></div>
-      </Flex>
-    </Layout>
+      <Layout>
+        <Heading fontSize={{ sm: '3xl' }} margin={2} marginLeft={4}>
+          {post.title}
+        </Heading>
+        <div>
+          {post.tags.map(tag => (
+            <React.Fragment key={tag.id}>
+              <Tag marginLeft={6} marginTop={1} size={'sm'}>
+                {tag.name}
+              </Tag>
+            </React.Fragment>
+          ))}
+        </div>
+        <Image
+          src={post.image.url}
+          rounded="lg"
+          margin={4}
+          marginBottom={[10, 20, 20, 20]}
+        />
+        <Flex margin="6">
+          <div dangerouslySetInnerHTML={{ __html: `${post.content}` }}></div>
+        </Flex>
+      </Layout>
+    </>
   );
 };
 
--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -3,26 +3,36 @@ import { withTheme } from 'emotion-theming';
 import { Image, Flex } from '@chakra-ui/core';
 
 import Layout from '../components/templates/Layout';
+import HeadComponent from '../components/templates/Head';
 
 const SuccessPage: React.FC = () => (
-  <Layout>
-    <Flex position="relative" margin="0 auto">
-      <Image
-        src="/hukidashi.png"
-        width={[56, 56, 56, 56]}
-        height={[64, 64, 64, 64]}
-        position="absolute"
-      />
-      <Image
-        src="/ryusou2.png"
-        width={[48, 48, 48, 48]}
-        height={[64, 64, 64, 64]}
-        position="relative"
-        left="24"
-        top="24"
-      />
-    </Flex>
-  </Layout>
+  <>
+    <HeadComponent
+      title={'RyusouBlog'}
+      description={'お問い合わせ完了ページです'}
+      keyword={'Home'}
+      image={'./hero.png'}
+      url={'https://ryusou-blog.now.sh/success'}
+    />
+    <Layout>
+      <Flex position="relative" margin="0 auto">
+        <Image
+          src="/hukidashi.png"
+          width={[56, 56, 56, 56]}
+          height={[64, 64, 64, 64]}
+          position="absolute"
+        />
+        <Image
+          src="/ryusou2.png"
+          width={[48, 48, 48, 48]}
+          height={[64, 64, 64, 64]}
+          position="relative"
+          left="24"
+          top="24"
+        />
+      </Flex>
+    </Layout>
+  </>
 );
 
 export default withTheme(SuccessPage);
