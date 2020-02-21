9ca137daa097149bf060e3d94e92604b8ddf3462
Youhei Nozaki <youhey.hey0505@gmail.com>
2020-02-21 12:11:19 +0900

    Static exportの設定をnext.config.jsに設定

--- next.config.js
+++ next.config.js
@@ -1,6 +1,35 @@
+const axios = require('axios');
 require('dotenv').config();
 
 module.exports = {
+  exportPathMap: async function() {
+    const paths = {
+      '/': { page: '/' },
+      '/contact': { page: '/contact' },
+      '/work': { page: '/work' },
+      '/success': { page: '/success' },
+    };
+
+    const key = {
+      headers: {
+        'Content-Type': 'application/json',
+        'X-API-KEY': process.env.API_KEY,
+      },
+    };
+    const res = await axios.get(
+      `https://ryusou-mtkh.microcms.io/api/v1/posts/`,
+      key,
+    );
+    const data = await res.data.contents;
+
+    data.forEach(post => {
+      paths[`/posts/${post.id}`] = {
+        page: '/posts/[id]',
+        query: { id: post.id },
+      };
+    });
+    return paths;
+  },
   env: {
     api_key: process.env.API_KEY,
     x_api_key: process.env.X_API_KEY,
