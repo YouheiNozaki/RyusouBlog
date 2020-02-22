96e567c06179a277132fb4baf11f304c9a1133d6
Youhei Nozaki <youhey.hey0505@gmail.com>
2020-02-22 23:36:00 +0900

    記事の個別ページにシェアボタンを設置

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0a63e6d11edccbe93960";
+/******/ 	var hotCurrentHash = "752f8368a9b1c855f70c";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- src/pages/posts/[id].tsx
+++ src/pages/posts/[id].tsx
@@ -1,7 +1,15 @@
 import * as React from 'react';
 import { NextPage } from 'next';
+import {
+  LineIcon,
+  LineShareButton,
+  TwitterIcon,
+  TwitterShareButton,
+  FacebookIcon,
+  FacebookShareButton,
+} from 'react-share';
 import { withTheme } from 'emotion-theming';
-import { Heading, Tag, Image, Flex } from '@chakra-ui/core';
+import { Heading, Tag, Image, Flex,Box } from '@chakra-ui/core';
 
 import { axiosInstance } from '../../lib/api';
 import { IPost } from '../../interfaces';
@@ -35,6 +43,32 @@ const PostContent: NextPage<Props> = ({ post }) => {
       />
       <Flex margin="6">
         <div dangerouslySetInnerHTML={{ __html: `${post.content}` }}></div>
+      </Flex>
+      <Flex margin={6} justify-content={"right"} position="fixed" right={2} bottom={2}>
+        <Box margin={2}>
+          <TwitterShareButton
+          title={post.title}
+          url={`https://ryusou-mtkh.now.sh/posts/${post.id}`}
+        >
+          <TwitterIcon size={32} round />
+        </TwitterShareButton>
+        </Box>
+        <Box margin={2}>
+        <FacebookShareButton
+          quote={post.title}
+          url={`https://ryusou-mtkh.now.sh/posts/${post.id}`}
+        >
+          <FacebookIcon size={32} round />
+        </FacebookShareButton>
+        </Box>
+        <Box margin={2}>
+        <LineShareButton
+          title={post.title}
+          url={`https://ryusou-mtkh.now.sh/posts/${post.id}`}
+        >
+          <LineIcon size={32} round />
+        </LineShareButton>
+        </Box>
       </Flex>
     </Layout>
   );
