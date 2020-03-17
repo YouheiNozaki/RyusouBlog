058b6eb70093f6bf65b288efaf7e24d52ed1a56f
Youhei Nozaki <youhey.hey0505@gmail.com>
2020-03-17 15:55:24 +0900

    types/rehype-reactを追加

--- src/lib/renderAst.ts
+++ src/lib/renderAst.ts
@@ -1,24 +1,7 @@
 import React from 'react';
-// @ts-ignore
-import rehypereact from 'rehype-react';
 import unified from 'unified';
-
-// @ts-ignore
-import parse from 'rehype-parse';
-// @ts-ignore
-import rehype2remark from 'rehype-remark';
-// @ts-ignore
-import stringify from 'remark-stringify';
+import rehypereact from 'rehype-react';
 
 const renderAst = new rehypereact({
   createElement: React.createElement,
 }).Compiler;
-
-export const processor = (post: string) => {
-  const tree = unified()
-    .use(parse)
-    .use(rehype2remark)
-    .use(renderAst)
-    .process(post);
-  return tree;
-};
